67b6931a6dee397d1f541c2777723af5ff31e331
Robert Novotny <novotnr0@gmail.com>
2021-04-28 14:46:27 +0200

    Pridame kostru pre obsluhu povolenia pre polohu

--- app/src/main/java/com/github/novotnyr/android/dolondyna/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/android/dolondyna/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.github.novotnyr.android.dolondyna
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 
 class MainActivity : AppCompatActivity() {
+    private val locationPermission =
+        registerForActivityResult(RequestPermission()) { granted ->
+            if (granted) {
+                showLocation()
+            } else {
+                TODO("Povolenie bolo zamietnuté")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onCompassImageViewClick(view: View) {
+        when (checkSelfPermission(this, ACCESS_FINE_LOCATION)) {
+            PERMISSION_GRANTED -> showLocation()
+            else -> locationPermission.launch(ACCESS_FINE_LOCATION)
+        }
+    }
+
+    fun showLocation() {
+        TODO("Zobraziť polohu")
     }
 }
