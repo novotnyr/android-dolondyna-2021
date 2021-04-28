b97a48b91141bc575a271f6380d6ad7ff71a5c2d
Robert Novotny <novotnr0@gmail.com>
2021-04-28 23:24:27 +0200

    Pridana pomocna trieda na zistovanie polohy

---
+++ app/src/main/java/com/github/novotnyr/android/dolondyna/LocationManager.kt
@@ -0,0 +1,47 @@
+package com.github.novotnyr.android.dolondyna
+
+import android.content.Context
+import android.location.Location
+import android.os.Looper
+import android.util.Log
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import java.util.concurrent.TimeUnit
+
+class LocationManager(
+    context: Context,
+    locationListener: (Location) -> Unit = {}
+) {
+    private val locationProvider =
+        LocationServices.getFusedLocationProviderClient(context)
+
+    private val locationRequest = LocationRequest.create().apply {
+        interval = TimeUnit.SECONDS.toMillis(5)
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    private val locationCallback: LocationCallback =
+        object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult) {
+                locationListener(result.lastLocation)
+            }
+        }
+
+    fun start() {
+        try {
+            locationProvider.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        } catch (e: SecurityException) {
+            Log.e("LocationService", "Location access denied", e)
+        }
+    }
+
+    fun stop() {
+        locationProvider.removeLocationUpdates(locationCallback)
+    }
+}
