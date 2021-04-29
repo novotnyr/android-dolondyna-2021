d0021bf65df48e7760d0daf1d462910e36f0a1bd
Robert Novotny <novotnr0@gmail.com>
2021-04-29 02:14:18 +0200

    Pridanie spravcu notifikacii a notifikacneho kanala

--- app/src/main/java/com/github/novotnyr/android/dolondyna/LocationService.kt
+++ app/src/main/java/com/github/novotnyr/android/dolondyna/LocationService.kt
@@ -1,21 +1,32 @@
 package com.github.novotnyr.android.dolondyna
 
+import android.app.NotificationChannel
+import android.app.NotificationManager.IMPORTANCE_DEFAULT
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationManagerCompat
 
 const val TAG = "LocationService"
 
-class LocationService: Service() {
+const val CHANNEL_ID = "0"
+
+class LocationService : Service() {
     private lateinit var locationManager: LocationManager
+
+    private lateinit var notificationManager: NotificationManagerCompat
 
     override fun onCreate() {
         super.onCreate()
         locationManager = LocationManager(this) { location ->
-            val message = "%.2f:%.2f".format(location.latitude, location.longitude)
+            val message =
+                "%.2f:%.2f".format(location.latitude, location.longitude)
             Log.i(TAG, message)
         }
+        notificationManager = NotificationManagerCompat.from(this)
+        createNotificationChannel()
     }
 
     override fun onStartCommand(
@@ -34,5 +45,12 @@ class LocationService: Service() {
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
+        NotificationChannel(
+            CHANNEL_ID, "Aktuálna poloha", IMPORTANCE_DEFAULT
+        ).let(notificationManager::createNotificationChannel)
     }
 }
